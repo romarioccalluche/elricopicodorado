6350fc34d703466cfb18b559af076c549508ce66
romarioccalluche <ccallucher@gmail.com>
2024-07-09 21:30:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    //inicio
+    $('#p1').click(function(){
+    $('#contenido').load("inicio.html");});
+        
+    //nosotros
+    $('#p2').click(function(){
+    $('#contenido').load("nosotros.html");});
+
+    //galeria
+    $('#p3').click(function(){
+    $('#contenido').load("galeria.html");});
+
+
+    //pie de pagina
+    $('#pie1').click(function(){
+    $('#contenido').load("inicio.html");});
+        
+    $('#pie2').click(function(){
+    $('#contenido').load("nosotros.html");});
+
+    $('#pie3').click(function(){
+    $('#contenido').load("galeria.html");});
+});
+
---
+++ tarjetas.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    //aji de gallina
+    $('#t1').click(function(){
+    $('#contenido').load("ajigallina.html");});
+            
+    //picarones
+    $('#t2').click(function(){
+    $('#contenido').load("picarones.html");});
+    
+    //pollo al horno
+    $('#t3').click(function(){
+    $('#contenido').load("polloalhorno.html");});
+
+    //pollo a la braza
+    $('#t4').click(function(){
+        $('#contenido').load("pollobraza.html");});
+
+});
